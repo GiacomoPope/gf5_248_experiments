a1995f3202e48f46e630c17f97321e39083284a6
Giacomo Pope <giacomopope@gmail.com>
2026-04-15 17:08:44 +0100

    Add full set of tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,5 +14,67 @@ fp2::define_fp_core!(typename = FpGen, modulus = MODULUS,);
 #[cfg(test)]
 mod test_sqisign_i_arithmetic {
     use super::FpGen;
+
     fp2::define_fp_tests!(FpGen);
+}
+
+#[cfg(test)]
+mod test_gen_fp {
+    use super::FpGen;
+
+    fp2::define_fp_tests!(FpGen);
+}
+
+#[cfg(test)]
+mod test_gen_fp2_with_sop {
+    use super::FpGen;
+    use super::MODULUS;
+
+    fp2::define_fp2_from_type!(
+        typename = Fp2Gen,
+        base_field = FpGen,
+        use_sum_of_products = true,
+    );
+    fp2::define_fp2_tests!(Fp2Gen, MODULUS, 5);
+}
+
+
+#[cfg(test)]
+mod test_gen_fp2_without_sop {
+    use super::FpGen;
+    use super::MODULUS;
+
+    fp2::define_fp2_from_type!(
+        typename = Fp2Gen,
+        base_field = FpGen,
+        use_sum_of_products = false,
+    );
+    fp2::define_fp2_tests!(Fp2Gen, MODULUS, 5);
+}
+
+#[cfg(test)]
+mod test_specific_fp2_with_sop {
+    use crate::gf5_248::GF5_248;
+    use super::MODULUS;
+
+    fp2::define_fp2_from_type!(
+        typename = Fp2Spec,
+        base_field = GF5_248,
+        use_sum_of_products = true,
+    );
+    fp2::define_fp2_tests!(Fp2Spec, MODULUS, 5);
+}
+
+
+#[cfg(test)]
+mod test_specific_fp2_without_sop {
+    use crate::gf5_248::GF5_248;
+    use super::MODULUS;
+
+    fp2::define_fp2_from_type!(
+        typename = Fp2Spec,
+        base_field = GF5_248,
+        use_sum_of_products = false,
+    );
+    fp2::define_fp2_tests!(Fp2Spec, MODULUS, 5);
 }
